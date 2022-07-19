49637da07123d399258a6c46eea2592a0719f11d
joe135730 <a0953868706@gmail.com>
2022-07-19 15:25:33 +0800

    7/16 class work

---
+++ src/components/MainPage.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="musicPlayerWrapper">
+  <div class="musicMain">
+    <div class="musicLeftNavbar">
+      <div class="leftNavbarTop">
+        <div class="musicLogo">
+          <div class="logo"></div>
+          <p>Music Box</p>
+        </div>
+        <div class="musicCategory">
+          <ul>
+            <li>
+              <div class="icon"></div>
+              <p>Home</p>
+            </li>
+            <li>
+              <div class="icon"></div>
+              <p>Discover</p>
+            </li>
+            <li>
+              <div class="icon"></div>
+              <p>Radio</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="leftNavbarMiddle"></div>
+      <div class="leftNavbarBottom"></div>
+    </div>
+    <div class="musicContent"></div>
+    <div class="musicRightNavbar"></div>
+  </div>
+
+  <div class="musicFooter"></div>
+
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.musicLogo {
+
+}
+
+.musicCategory {
+
+}
+
+li {
+  display: flex;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 6px;
+  background: #000;
+}
+
+</style>
